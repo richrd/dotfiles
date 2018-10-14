df1ee951bd654cd25dd145923ced8348d04281e5
Richard Lewis <richrd.lewi@gmail.com>
2018-10-14 21:49:46 +0300

    add package install script and sublime settings

---
+++ code/shell/install-basic-packages.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Install some basic packages that I always use
+
+sudo apt install \
+    git \
+    tig \
+    htop \
+    redshift \
+    redshift-gtk \
+    xclip \
+    zsh \
+
+
+
+
+
